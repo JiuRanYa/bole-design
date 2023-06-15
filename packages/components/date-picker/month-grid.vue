c8aecf7e480c94c97e33f45ef93b06f3ea47c93c
1084350607 <1084350607@qq.com>
2023-06-15 11:09:47 +0800

    fix: month grid refactor

--- packages/components/date-picker/month-grid.vue
+++ packages/components/date-picker/month-grid.vue
@@ -20,7 +20,6 @@ const props = useProps('month-grid', _props, {
   value: ''
 })
 
-const selectedDate = ref()
 const tableRef = ref<HTMLElement>()
 const now = dayjs().format('YYYY-MM-DD')
 
@@ -62,7 +61,7 @@ function getWeekDayByDate(date: string) {
   return dayjs(date).day()
 }
 
-function calcDate(day: string) {
+function calcDateByDay(day: string) {
   if (!props.value || !day) return ''
   return dayjs(`${props.value}-${day}`).format('YYYY-MM-DD')
 }
@@ -73,7 +72,6 @@ function handlePickDate(e: Event) {
 
   const day = target.ariaLabel
   const date = dayjs(`${props.value}-${day}`)
-  selectedDate.value = calcDate(day)
 
   const emitValue = {
     year: date.year(),
@@ -82,7 +80,20 @@ function handlePickDate(e: Event) {
   }
   emit('pick', emitValue)
 }
+function getCellClass(row: number, col: number) {
+  const res = []
+  const date = calcDateByDay(getDayAriaLabel(row, col))
 
+  if (now === date) {
+    res.push('today')
+  }
+
+  if (selectedDate.value === date) {
+    res.push('selected')
+  }
+
+  return res
+}
 defineExpose({ tableRef })
 </script>
 
@@ -99,10 +110,7 @@ defineExpose({ tableRef })
           part="data"
           :aria-label="getDayAriaLabel(i, j)"
           v-for="j in 7"
-          :class="{
-            today: now === calcDate(getDayAriaLabel(i, j)),
-            'selected-date': selectedDate === calcDate(getDayAriaLabel(i, j))
-          }"
+          :class="getCellClass(i, j)"
           :aria-colindex="j"
         >
           {{ getDayAriaLabel(i, j) }}
