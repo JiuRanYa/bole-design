4405cc268ec5759a17a2e8585b977278eb4fc077
1084350607 <1084350607@qq.com>
2023-07-28 12:23:32 +0800

    pref: optmize bole design packages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,10 @@ import Comp from 'unplugin-vue-components/vite'
 import markdownTransform from './.vitepress/plugins/markdownTransform'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import DefineOptions from 'unplugin-vue-define-options/vite'
-import path from 'node:path'
+import path, { resolve } from 'node:path'
+import { readFileSync } from 'node:fs'
+
+const pkg = JSON.parse(readFileSync(resolve(__dirname, '../package.json'), 'utf-8'))
 
 export default defineConfig(() => {
   return {
@@ -30,6 +33,12 @@ export default defineConfig(() => {
       alias: {
         '@/': `${path.resolve(__dirname, '')}/`
       }
+    },
+    optimizeDeps: {
+      include: [
+        '@bole-design/icons',
+        ...Object.keys(pkg.dependencies).filter((dep: string) => !dep.includes('bole-design'))
+      ]
     },
     server: {
       host: true
