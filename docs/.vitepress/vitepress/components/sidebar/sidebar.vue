cfcf72eb761e23564817108fa2709ed26f076474
1084350607 <1084350607@qq.com>
2023-08-09 17:02:48 +0800

    refactor: content layout

--- docs/.vitepress/vitepress/components/sidebar/sidebar.vue
+++ docs/.vitepress/vitepress/components/sidebar/sidebar.vue
@@ -15,23 +15,21 @@ const { sidebars } = useSidebar()
 </script>
 
 <template>
-  <div v-if="hasSidebar" class="bl-sidebar">
-    <aside>
-      <div class="sidebar-groups">
-        <section v-for="(item, key) of sidebars" :key="key" class="sidebar-group">
-          <p class="sidebar-group__title">
-            {{ item.text }}
-          </p>
-          <SidebarLink v-for="(child, childKey) in item.children" :key="childKey" :item="child" />
-        </section>
-      </div>
-    </aside>
-  </div>
+  <aside v-if="hasSidebar" class="bl-sidebar">
+    <div class="sidebar-groups">
+      <section v-for="(item, key) of sidebars" :key="key" class="sidebar-group">
+        <p class="sidebar-group__title">
+          {{ item.text }}
+        </p>
+        <SidebarLink v-for="(child, childKey) in item.children" :key="childKey" :item="child" />
+      </section>
+    </div>
+  </aside>
 </template>
 
 <style lang="scss" scoped>
 .bl-sidebar {
-  width: 300px;
+  width: 240px;
   height: 100%;
   padding: 48px 32px 96px;
   box-sizing: border-box;
